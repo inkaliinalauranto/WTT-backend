f58f770c41ee673c99684c33495ffe25d3f15c2b
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-11-11 20:37:37 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,31 +19,35 @@ app.include_router(auth.router)
 
 
 # :D
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-def index():
-    return """
-        <html>
-          <head>
-            <style>
-              .button {
-                background-color: #04AA6D;
-                display: block;
-                border: none;
-                max-width: 500px;
-                font-size: 28px;
-                text-align: center;
-                color: #FFFFFF;
-                padding: 20px;
-                transition-duration: 0.4s;
-                text-decoration: none;
-              }
-              .button:hover {
-                background: #03793D;
-              }
-            </style>
-          </head>
-          <body style="align-items: centered">
-            <a class="button" href="/docs">Open Fastapi Swagger UI</a>
-          </body>
-        </html>
-    """
+#@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+#def index():
+#    return """
+#        <html>
+#          <head>
+#            <style>
+#              .button {
+#                background-color: #04AA6D;
+#                display: block;
+#                border: none;
+#                max-width: 500px;
+#                font-size: 28px;
+#                text-align: center;
+#                color: #FFFFFF;
+#                padding: 20px;
+#                transition-duration: 0.4s;
+#                text-decoration: none;
+#              }
+#              .button:hover {
+#                background: #03793D;
+#              }
+#            </style>
+#          </head>
+#          <body style="align-items: centered">
+#            <a class="button" href="/docs">Open Fastapi Swagger UI</a>
+#          </body>
+#        </html>
+#    """
+
+@app.get("/")
+def read_roots():
+    return {"message": "Hello, FastAPI!!!!!!!!!!"}
