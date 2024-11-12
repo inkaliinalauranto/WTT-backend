4e352f31ab1b34a26619c6518b5e745f0486961b
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-11-12 18:01:41 +0200

    Fix tables

--- app/models.py
+++ app/models.py
@@ -12,14 +12,14 @@ Base = declarative_base()
 
 
 class Test(Base):
-    __tablename__ = "test"
+    __tablename__ = "tests"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True)
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(45), nullable=False, unique=True)
@@ -30,49 +30,45 @@ class User(Base):
     access_jti = Column(String(512))
     created_at = Column(TIMESTAMP, nullable=False)
     deleted_at = Column(TIMESTAMP)
-    role_id = Column(Integer, ForeignKey("role.id"))
-    team_id = Column(Integer, ForeignKey("team.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+    team_id = Column(Integer, ForeignKey("teams.id"))
 
 
 class Role(Base):
-    __tablename__ = 'role'
+    __tablename__ = 'roles'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(45), nullable=False, unique=True)
-
-    role_id = relationship('User', back_populates='role_id')
 
 
 class Team(Base):
-    __tablename__ = 'team'
+    __tablename__ = 'teams'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(45), nullable=False, unique=True)
-    organization_id = Column(Integer, ForeignKey("organization.id"))
-
-    team_id = relationship('User', back_populates='team_id')
+    organization_id = Column(Integer, ForeignKey("organizations.id"))
 
 
 class Organization(Base):
-    __tablename__ = 'organization'
+    __tablename__ = 'organizations'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(45), nullable=False, unique=True)
 
 
 class ShiftType(Base):
-    __tablename__ = "shift_type"
+    __tablename__ = "shift_types"
 
     id = Column(Integer, primary_key=True, index=True)
     type = Column(String(45), nullable=False, unique=True)
 
 
 class Shift(Base):
-    __tablename__ = "shift"
+    __tablename__ = "shifts"
 
     id = Column(Integer, primary_key=True, index=True)
     start_time = Column(TIMESTAMP, nullable=False)
     end_time = Column(TIMESTAMP, nullable=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    shift_type_id = Column(Integer, ForeignKey("shift_type.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    shift_type_id = Column(Integer, ForeignKey("shift_types.id"))
     description = Column(String(255), nullable=True)
