dcc993e8294954ca7b88691b558d8f33531bff05
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-11-19 02:04:13 +0200

    Vaihdettu statuskoodit forbidden, lisätty keksitarkastus ja vaihtoehtosesti authorization headerista tokenin tarkastus jos keksiä ei ole

--- app/utils/logged_in_user.py
+++ app/utils/logged_in_user.py
@@ -1,5 +1,6 @@
+from os import access
 from typing import Annotated
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from app import models
@@ -11,25 +12,31 @@ from app.utils.access_token import Token
 oauth_scheme = OAuth2PasswordBearer(tokenUrl='/api/auth/login/openapi', auto_error=False)
 
 
-def get_user_by_access_jti(token: Token, service: UsersServ, auth: Annotated[str, Depends(oauth_scheme)] = None):
+def get_user_by_access_jti(token: Token, service: UsersServ, req: Request, auth: Annotated[str, Depends(oauth_scheme)] = None):
+    # Asetetaan access_tokeniksi keksistä löytyvä token
+    # Ja käytetään sitä ensisijaisesti. Vaihtoehtoisesti tarkastetaan, tuliko authorization bearerin mukana token.
+    access_token = req.cookies.get("wtt-cookie")
+
     # Tarkistetaan, löytyykö Authorization Bearer token.
-    if not auth:
-        raise HTTPException(status_code=401, detail="authorization not found")
+    if not access_token:
+        access_token = auth
+        if not auth:
+            raise HTTPException(status_code=401, detail="Unauthorized access")
 
     try:
         # Tarkistetaan onko token meidän luoma
-        payload = token.verify(auth)
+        payload = token.verify(access_token)
         if payload['type'] != 'access':
-            raise HTTPException(status_code=401, detail="unauthorized access")
+            raise HTTPException(status_code=401, detail="Unauthorized access")
 
         # Tarkistetaan onko käyttäjä kirjautunut sisään
         user = service.get_user_by_access_jti(payload['sub'])
         if user is None:
-            raise HTTPException(status_code=404, detail="user not found")
+            raise HTTPException(status_code=401, detail="Unauthorized access")
 
     # Token ei ole validi
     except JWTError:
-        raise HTTPException(status_code=401, detail="unauthorized access")
+        raise HTTPException(status_code=401, detail="Unauthorized access")
 
     # models.User
     return user
