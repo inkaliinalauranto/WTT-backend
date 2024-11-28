0355990043ff05f63f39146564d53bdb25479d09
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-11-28 16:40:23 +0200

    Luotu organisaation luontifunktio

--- app/services/base_services/organizations_base_service.py
+++ app/services/base_services/organizations_base_service.py
@@ -6,3 +6,7 @@ class OrganizationsBaseService(abc.ABC, BaseService):
     @abc.abstractmethod
     def get_by_id(self, org_id: int):
         raise NotImplemented()
+
+    @abc.abstractmethod
+    def create_org_if_not_exist(self, org_name: str):
+        raise NotImplemented()
